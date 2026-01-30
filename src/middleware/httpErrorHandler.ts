106b4f7c4bc0f4ea82a4475eb6b6a57ce2e654bf
LucasNatoli <lucas.natoli@gmail.com>
2026-01-30 17:07:45 -0300

    separar el httpErroHanlder y armar el barrel

---
+++ src/middleware/httpErrorHandler.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  Logger,
+  HttpError,
+  CONFLICT,
+  FORBIDDEN,
+  UNPROCESSABLE,
+  UNAUTHORIZED,
+  sleep,
+  optionalEnv,
+} from "../commons";
+
+export async function httpErrorHandler(
+  error: HttpError,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  const logtoconsole = optionalEnv("LOG_TO_CONSOLE", false);
+  const { status, message } = { ...error };
+  const proc = `${req.method} ${req.originalUrl} ${status} ${req.ip}`;
+  const response = { success: false, message };
+  const logger = new Logger("http");
+  switch (status) {
+    case UNAUTHORIZED.status:
+    case UNPROCESSABLE.status:
+      logger.warn(proc, message);
+      await sleep(optionalEnv("AUTH_DELAY", 3000));
+      res.status(status).json(response);
+      break;
+    case CONFLICT.status:
+      logger.warn(proc, message);
+      res.status(status).json(response);
+      break;
+    case FORBIDDEN.status:
+      logger.warn(proc, message);
+      res.status(status).json(response);
+      break;
+    default:
+      logger.error(proc, message);
+      res.status(status).json(response);
+      break;
+  }
+  if (logtoconsole) console.error(proc, message);
+}
