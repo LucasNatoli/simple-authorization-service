f4ac0acc0a673d83dc01658d2a8ce3865ccf0e7c
Lucas Natoli <lucas.natoli@gmail.com>
2026-02-01 23:46:39 -0300

    agregar el middleware de errores a la cadena de rutas como default

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
 import { checkToken, register, login } from "./accounts";
-
+import { httpErrorHandler } from "../middleware";
 /**
  * Agregar las rutas de manejo de usuarios
  *
@@ -8,7 +8,8 @@ import { checkToken, register, login } from "./accounts";
  * @param {Express} app
  */
 export function router(app: Express) {
-  app.post( "/register", register);
-  app.post( "/login", login);
-  app.get("/check", checkToken, (_req, res, _next)=>{res.status(200).send()})
+  app.post("/register", register);
+  app.post("/login", login);
+  app.get("/check", checkToken, (_req, res, _next) => { res.status(200).send() })
+  app.use(httpErrorHandler);
 }
